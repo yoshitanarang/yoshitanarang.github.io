90ac923f149088e715151cb55ac506e5bfbdcb18
yosh <yoshitanarang14@gmail.com>
2022-09-18 14:13:34 -0500

    initial commit

---
+++ src/views/home.js
@@ -0,0 +1,253 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="home-home">
+        <div className="home-about">
+          <div className="home-info">
+            <span className="home-text">
+              <span>About me 📱</span>
+            </span>
+            <span className="home-text02">
+              <span>
+                A strong-willed, supersonic learner &amp; an aspiring Machine
+                Learning Engineer with strengths in communication and innovative
+                thinking. I execute a growth mindset in my work so that I can
+                grasp and adopt new cutting-edge technologies and develop
+                people-centered solutions.
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+            </span>
+            <span className="home-text04">
+              <span>
+                My passion projects involve competing at hackathons in DFW
+                metroplex, &amp; attending Society of Women Engineers
+                conferences across the US. This past summer, I volunteered for
+                48in48 to design &amp; develop a website for a nonprofit
+                organization. Fun fact about me: I’m an absolute nerd for real
+                estate 🏡
+              </span>
+            </span>
+          </div>
+          <img
+            alt="yoshiImg11147"
+            src="/playground_assets/yoshiimg11147-887-500w.png"
+            className="home-yoshi-img1"
+          />
+        </div>
+        <div className="home-group2">
+          <div className="home-project">
+            <img
+              alt="Line1I111"
+              src="/playground_assets/line1i111-4ah.svg"
+              className="home-line1"
+            />
+            <div className="home-frame5">
+              <div className="home-frame4">
+                <div className="home-frame3">
+                  <span className="home-text06">
+                    <span>UT Dallas x WeHack Hackathon 2022</span>
+                  </span>
+                  <span className="home-text08">
+                    <span>Toyota Electrified On-the-Go</span>
+                  </span>
+                </div>
+                <div className="home-movingarrow">
+                  <div className="home-maskgroup">
+                    <img
+                      alt="Rectangle4I111"
+                      src="/playground_assets/rectangle4i111-5oa3-200h.png"
+                      className="home-rectangle4"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="home-projectimage"></div>
+            </div>
+          </div>
+          <div className="home-project1">
+            <img
+              alt="Line1I111"
+              src="/playground_assets/line1i111-wstg.svg"
+              className="home-line11"
+            />
+            <div className="home-frame51">
+              <a
+                href="https://devpost.com/software/utd-building-classifier"
+                target="_blank"
+                rel="noreferrer noopener"
+                className="home-link"
+              >
+                <div className="home-frame41">
+                  <div className="home-frame31">
+                    <span className="home-text10">
+                      <span>UT Dallas x HackReason Hackathon 2022</span>
+                    </span>
+                    <span className="home-text12">
+                      <span>UT Dallas Building Classifier</span>
+                    </span>
+                  </div>
+                  <div className="home-movingarrow1">
+                    <div className="home-maskgroup1">
+                      <img
+                        alt="Rectangle4I111"
+                        src="/playground_assets/rectangle4i111-dzoo-200h.png"
+                        className="home-rectangle41"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </a>
+              <div className="home-projectimage1"></div>
+            </div>
+          </div>
+          <div className="home-project2">
+            <img
+              alt="Line1I111"
+              src="/playground_assets/line1i111-is9.svg"
+              className="home-line12"
+            />
+            <div className="home-frame52">
+              <div className="home-frame42">
+                <div className="home-frame32">
+                  <span className="home-text14">
+                    <span>My Resume</span>
+                  </span>
+                  <span className="home-text16">
+                    <span>Designed with Figma!</span>
+                  </span>
+                </div>
+                <div className="home-movingarrow2">
+                  <div className="home-maskgroup2">
+                    <img
+                      alt="Rectangle4I111"
+                      src="/playground_assets/rectangle4i111-tkhg-200h.png"
+                      className="home-rectangle42"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="home-projectimage2"></div>
+            </div>
+          </div>
+        </div>
+        <div className="home-group3">
+          <span className="home-text18">
+            <span className="home-text19">ML Enthusiast </span>
+            <span className="home-text20">
+              passionate about creating engaging, delightful experiences.
+            </span>
+            <br className="home-text21"></br>
+            <br className="home-text22"></br>
+            <span className="home-text23">
+              Currently studying Data Science
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span className="home-text24">@UT Dallas</span>
+            <span className="home-text25">
+               ☄️ &amp; an ML Intern @
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span className="home-text26">ZCLAP</span>
+            <span className="home-text27"> 📦</span>
+            <span className="home-text28">
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <br className="home-text29"></br>
+            <br></br>
+          </span>
+          <span className="home-text31">
+            <span>Hello, world 👋 I&apos;m Yoshita.</span>
+          </span>
+        </div>
+        <div className="home-gotopbtn">
+          <div className="home-movingarrow3">
+            <div className="home-maskgroup3"></div>
+            <div className="home-maskgroup4">
+              <img
+                alt="Rectangle4I112"
+                src="/playground_assets/rectangle4i112-lsi-200h.png"
+                className="home-rectangle43"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="home-header">
+          <div className="home-navbar">
+            <span className="home-text33">
+              <span>Home</span>
+            </span>
+            <a
+              href="https://drive.google.com/file/d/1RuOa5IQ7UFarQRGaxqEpbk_xCbBVBiIF/view?usp=sharing"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="home-text35"
+            >
+              <span>Resume</span>
+            </a>
+          </div>
+        </div>
+        <span className="home-text37">
+          <span>Yoshita Narang</span>
+        </span>
+        <span className="home-text39">
+          <span>Machine Learning Intern</span>
+        </span>
+        <img
+          alt="TOYOTAPITCH11076"
+          src="/playground_assets/toyotapitch11076-xw4-600h.png"
+          className="home-t-o-y-o-t-a-p-i-t-c-h1"
+        />
+        <a
+          href="https://devpost.com/software/utd-building-classifier"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link1"
+        >
+          <img
+            alt="Janruary15202211077"
+            src="/playground_assets/janruary15202211077-hi1v-600h.png"
+            className="home-janruary1520221"
+          />
+        </a>
+        <img
+          alt="Resume11077"
+          src="/playground_assets/resume11077-5bzb-600h.png"
+          className="home-resume1"
+        />
+        <img
+          alt="memoji411147"
+          src="/playground_assets/memoji411147-1rq-400h.png"
+          className="home-memoji41"
+        />
+        <span className="home-text41">© 2022 Yoshita Narang</span>
+      </div>
+    </div>
+  )
+}
+
+export default Home
